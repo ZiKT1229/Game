35eea6362f1035bca5d1a9129342a26e8af4aedc
KT_Zheng <a6163508@gmail.com>
2018-11-10 17:45:55 +0800

    增加snake移動的動畫function

--- snake/snake.js
+++ snake/snake.js
@@ -1,16 +1,39 @@
-var canvase = document.getElementById("game");
+var canvas = document.getElementById("game");
 var context = canvas.getContext('2d');
 const grid = 16;
 var count = 0;
 
 class Snake {
     constructor() {
-        this._x = 0;
-        this._y = 0;
+        this._x = getRandomInt(0, 25) * grid;
+        this._y = getRandomInt(0, 25) * grid;
         this._dx = grid;
         this._dy = 0;
         this._cells = [];
         this._length = 1;
+    }
+    drawSnake() {
+        context.fillStyle = 'green';
+        this._cells.forEach(function(cell, index) { //有些問題，關於this的使用
+            context.fillRect(cell.x, cell.y, grid-1, grid-1);
+
+            if (cell.x === apple.x && cell.y === apple.y) {
+                snake._length++;
+                apple.newApple();
+            }
+            
+            for (let i = index + 1; i < snake._cells.length; i++) {
+                if (cell.x === snake._cells[i].x && cell.y === snake._cells[i].y) {
+                    snake._x = getRandomInt(0, 25) * grid;
+                    snake._y = getRandomInt(0, 25) * grid;
+                    snake._cells = [];
+                    snake._length = 1;
+                    snake._dx = grid;
+                    snake._dy = 0;
+                    apple.newApple();
+                }
+            }
+        });
     }
     move() {
         this._x += this._dx;
@@ -32,16 +55,37 @@ class Snake {
             this._cells.pop();
         }
     }
+    turn(direction) {
+        if (direction === 37 && this._dx === 0) {
+            this._dx = -grid;
+            this._dy = 0;
+        }
+        // up arrow key
+        else if (direction === 38 && this._dy === 0) {
+            this._dy = -grid;
+            this._dx = 0;
+        }
+        // right arrow key
+        else if (direction === 39 && this._dx === 0) {
+            this._dx = grid;
+            this._dy = 0;
+        }
+        // down arrow key
+        else if (direction === 40 && this._dy === 0) {
+            this._dy = grid;
+            this._dx = 0;
+        }
+    }
 }
 
 class Apple {
     constructor() {
-        this._x = getRandomInt(0, 25);
-        this._y = getRandomInt(0, 25);
+        this._x = getRandomInt(0, 25) * grid;
+        this._y = getRandomInt(0, 25) * grid;
     }
     newApple() {
-        this._x = getRandomInt(0, 25);
-        this._y = getRandomInt(0, 25);
+        this._x = getRandomInt(0, 25) * grid;
+        this._y = getRandomInt(0, 25) * grid;
     }
     drawApple() {
         context.fillStyle = 'red';
@@ -58,3 +102,27 @@ class Apple {
 const getRandomInt = (min, max) => {
     return Math.floor(Math.random() * (max - min)) + min;
 }
+
+const animation = () => {
+    requestAnimationFrame(animation);
+    if (++count < 4) {
+        return;
+    }
+
+    count = 0;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    snake.move();
+
+    apple.drawApple();
+
+    snake.drawSnake();
+};
+
+document.addEventListener('keydown', function(e) {
+    snake.turn(e.which);
+});
+
+var snake = new Snake();
+var apple = new Apple();
+requestAnimationFrame(animation);
