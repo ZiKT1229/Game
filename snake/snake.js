736f485ce9b8b9ea65b3905a53b18a5840e618b3
KT_Zheng <a6163508@gmail.com>
2018-11-10 16:51:18 +0800

    增加class function

--- snake/snake.js
+++ snake/snake.js
@@ -12,11 +12,49 @@ class Snake {
         this._cells = [];
         this._length = 1;
     }
+    move() {
+        this._x += this._dx;
+        this._y += this._dy;
+        if (this._x < 0) {
+            this._x = canvas.width - grid;
+        }
+        else if (this._x >= canvas.width) {
+            this._x = 0;
+        }
+        if (this._y < 0) {
+            this._y = canvas.width - grid;
+        }
+        else if (this._y >= canvas.width) {
+            this._y = 0;
+        }
+        this._cells.unshift({x: this._x, y: this._y});
+        if (this._cells.length > this._length) {
+            this._cells.pop();
+        }
+    }
 }
 
 class Apple {
     constructor() {
-        this._x = 160;
-        this._y = 160;
+        this._x = getRandomInt(0, 25);
+        this._y = getRandomInt(0, 25);
     }
+    newApple() {
+        this._x = getRandomInt(0, 25);
+        this._y = getRandomInt(0, 25);
+    }
+    drawApple() {
+        context.fillStyle = 'red';
+        context.fillRect(this._x, this._y, grid-1, grid-1);
+    }
+    get x() {
+        return this._x;
+    }
+    get y() {
+        return this._y;
+    }
+}
+
+const getRandomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min)) + min;
 }
